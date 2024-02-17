63dbe39a8378ebd6c30f92313fb95cc9cbb31dbd
vimalsingh <1234>
2024-02-18 01:19:11 +0530

    Vimal Singh - 18th Feb 2024

--- src/main/java/VimalSingh/LevelByLevelTraversal.java
+++ src/main/java/VimalSingh/LevelByLevelTraversal.java
@@ -21,11 +21,31 @@ public class LevelByLevelTraversal {
         node.right = new BinaryNode(30);
         node.right.right= new BinaryNode(60);
 
-        levelByLevelTraversal(node);
+        levelByLevelTraversalMethod1(node);
+        System.out.println("method 2");
+        levelByLevelTraversalMethod2(node);
     }
 
-
-    private static void levelByLevelTraversal(BinaryNode node) {
+    /*
+            in the first iteration queue.add(10) and q.add(null) so q is not null
+            print 10 i.e curr.key
+            q.add(20) and q.add(30)
+            in the 2nd iteration queue is empty because null appears
+            so, in queue we have now 20,30,null
+            print 20
+            q.add(40), q.add(50)
+            now Queue become 30,null,40,50
+            third iteration
+            curr is not null so
+            print 30
+            then q.add(60)
+            now queue become null,40,50,60
+            then 4th iteration
+            print 40 then 50
+            q.add(null), q.add(70),q,add(80)
+            print 70,80
+         */
+    private static void levelByLevelTraversalMethod1(BinaryNode node) {
 
         Queue<BinaryNode> queue = new LinkedList<>();
         queue.add(node);
@@ -48,24 +68,29 @@ public class LevelByLevelTraversal {
         }
     }
 
-/*
-        in the first iteration queue.add(10) and q.add(null) so q is not null
-        print 10 i.e curr.key
-        q.add(20) and q.add(30)
-        in the 2nd iteration queue is empty because null appears
-        so, in queue we have now 20,30,null
-        print 20
-        q.add(40), q.add(50)
-        now Queue become 30,null,40,50
-        third iteration
-        curr is not null so
-        print 30
-        then q.add(60)
-        now queue become null,40,50,60
-        then 4th iteration
-        print 40 then 50
-        q.add(null), q.add(70),q,add(80)
-        print 70,80
-     */
+    private static void levelByLevelTraversalMethod2(BinaryNode node){
+        Queue<BinaryNode> queue = new LinkedList<>();
+        queue.add(node);
+        int size;
+        BinaryNode curr;
+        while (!queue.isEmpty()){
+            size=queue.size();
+            for (int i = 0; i < size; i++) {
+                curr = queue.poll();
+                assert curr != null;
+                System.out.println(curr.key);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+        }
+    }
+
+
+
+
 
 }
